55f7853618fa8d31d9787bb0fc5d351dc8c3d8d9
Maxim Predybailo <predmaxim@ya.ru>
2022-10-22 15:49:43 +0500

    refactor: vars & clear code

--- rss-gem-puzzle/src/main.js
+++ rss-gem-puzzle/src/main.js
@@ -7,11 +7,23 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   class Game {
+    itemSize = 90;
+    playField = [];
+    isGame = false;
+    time = 0;
+    canvas = document.createElement('canvas');
+    ctx = this.canvas.getContext('2d');
+    info = {
+      frameSize: 4,
+      moves: 0,
+      coords: {},
+      currentTime: { minutes: 0, seconds: 0 },
+      best: { time: { minutes: 0, seconds: 0 }, moves: 0, },
+    };
+
+
     constructor(setting) {
       this.setting = setting;
-      this.frameSize = 4;
-      this.itemSize = 90;
-      this.playField = [];
       this.movesElem = document.querySelector(this.setting.moves);
       this.wrap = document.querySelector(this.setting.wrap);
       this.startBtn = document.querySelector(this.setting.startBtn);
@@ -19,22 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
       this.saveBtn = document.querySelector(this.setting.saveBtn);
       this.resultBtn = document.querySelector(this.setting.resultBtn);
       this.timerElem = document.querySelector(this.setting.timerElem);
-      this.frameSizeInfo = document.querySelector(this.setting.frameSizeInfo);
-      this.frameSizeBtns = document.querySelectorAll(this.setting.frameSizeBtns);
-      this.canvas = document.createElement('canvas');
-      this.ctx = this.canvas.getContext('2d');
-      this.isGame = false;
-      this.time = 0;
-      this.info = {
-        best: {
-          time: { minutes: 0, seconds: 0 },
-          moves: 0,
-        },
-        moves: 0,
-        currentTime: { minutes: 0, seconds: 0 },
-        frameSize: 0,
-      };
-
+      this.info.frameSizeInfo = document.querySelector(this.setting.frameSizeInfo);
       this.init();
     }
 
@@ -45,7 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
       window.addEventListener('resize', () => {
         if (window.innerWidth < 1024) {
           1
-          this.itemSize = window.innerWidth / (this.frameSize + 2.5)
+          this.itemSize = window.innerWidth / (this.info.frameSize + 2.5)
           this.drawCanvas();
         } else this.itemSize = this.setting.itemSize
       })
@@ -59,9 +56,9 @@ document.addEventListener('DOMContentLoaded', () => {
         if (e.target == this.saveBtn) this.save();
         if (e.target == this.resultBtn) this.result();
         if (e.target.classList.contains('frame-size')) {
-          this.frameSize = +e.target.dataset.action;
-          this.frameSizeInfo.textContent = `${+e.target.dataset.action}x${+e.target.dataset.action}`;
-          this.info.frameSize = this.frameSize;
+          this.info.frameSize = +e.target.dataset.action;
+          this.info.frameSizeInfo.textContent = `${+e.target.dataset.action}x${+e.target.dataset.action}`;
+          this.info.frameSize = this.info.frameSize;
           this.stop();
           this.numbers();
           this.drawCanvas();
@@ -76,13 +73,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     numbers() {
 
-      const arr = Array(this.frameSize * this.frameSize)
+      const arr = Array(this.info.frameSize * this.info.frameSize)
         .fill().map((_, i) => i).sort(() => Math.random() - 0.5)
 
       const res = []
 
-      while (res.length < this.frameSize) {
-        res.push(arr.splice(0, this.frameSize));
+      while (res.length < this.info.frameSize) {
+        res.push(arr.splice(0, this.info.frameSize));
       }
       this.playField = res;
       return res
@@ -94,31 +91,45 @@ document.addEventListener('DOMContentLoaded', () => {
 
         this.timerElem.textContent = `${String(this.info.currentTime.minutes).length < 2 ? 0 : ''}${this.info.currentTime.minutes}:${String(this.info.currentTime.seconds).length < 2 ? 0 : ''}${this.info.currentTime.seconds}`;
 
-        this.frameSize = this.info.frameSize
-        this.frameSizeInfo.textContent = `${this.info.frameSize}x${this.info.frameSize}`;
+        this.info.frameSize = this.info.frameSize
+        this.info.frameSizeInfo.textContent = `${this.info.frameSize}x${this.info.frameSize}`;
       }
     }
 
-    drawCanvas() {      
+    move(x, y) {
+      if (x = 1) {
+
+      }
+    }
+
+    drawCanvas() {
 
       if (window.innerWidth < 1024) {
-        this.itemSize = window.innerWidth / (this.frameSize + 2.5)
+        this.itemSize = window.innerWidth / (this.info.frameSize + 2.5)
       } else this.itemSize = this.setting.itemSize
 
       this.ctx = this.canvas.getContext('2d')
       this.canvas.classList.add('game')
-      this.canvas.width = this.frameSize * this.itemSize
-      this.canvas.height = this.frameSize * this.itemSize
+      this.canvas.width = this.info.frameSize * this.itemSize
+      this.canvas.height = this.info.frameSize * this.itemSize
 
       this.wrap.insertAdjacentElement('afterbegin', this.canvas);
 
-      for (let i = 0; i < this.frameSize; i++) {
-        if (i == this.frameSize) i++
-        for (let j = 0; j < this.frameSize; j++) {
+      for (let i = 0; i < this.info.frameSize; i++) {
+        if (i == this.info.frameSize) i++
+        for (let j = 0; j < this.info.frameSize; j++) {
           this.ctx.save();
           this.ctx.translate(j * this.itemSize, i * this.itemSize);
-          this.ctx.strokeStyle = '#e2e2e2'
-          this.ctx.strokeRect(0, 0, this.itemSize, this.itemSize);
+          // this.ctx.strokeStyle = '#e2e2e2'
+          // this.ctx.strokeRect(0, 0, this.itemSize, this.itemSize);
+          this.ctx.fillStyle = '#e2e2e2'
+          this.ctx.fillRect(0, 0, this.itemSize - 4, this.itemSize - 4);
+
+          if (this.playField[i][j] == 0) {
+            this.ctx.fillStyle = '#fff'
+            this.ctx.fillRect(0, 0, this.itemSize - 4, this.itemSize - 4);
+          } else this.ctx.fillStyle = '#000'
+
           this.ctx.font = "20px sans-serif";
           this.ctx.textAlign = 'center';
           this.ctx.fillText(this.playField[i][j], this.itemSize / 2, this.itemSize / 2 + 8)
@@ -202,7 +213,9 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     newGame() {
-
+      this.canvas.addEventListener('click', (e) => {
+        console.log(e.clientX)
+      })
 
       return this
 
@@ -217,7 +230,6 @@ document.addEventListener('DOMContentLoaded', () => {
     resultBtn: '.result',
     timerElem: '.timer span',
     moves: '.moves span',
-    frameSizeBtns: '.frame-size',
     frameSizeInfo: '.frame-size-info span'
   })
 
